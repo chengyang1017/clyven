d7386610e7f23147541d6d5ce137a70335bf8b8f
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-12 22:57:38 +0800

    feat(i18n): localize topic labels safely

---
+++ apps/clyven_app/lib/core/localization/localized_labels.dart
@@ -0,0 +1,18 @@
+import 'package:clyven_app/l10n/app_localizations.dart';
+
+String localizedTopicLabel(
+  AppLocalizations l10n,
+  String topic,
+) {
+  return switch (topic) {
+    '全部' => l10n.topicAll,
+    '影像' => l10n.topicImage,
+    '技术' => l10n.topicTechnology,
+    '语言' => l10n.topicLanguage,
+    '游戏' => l10n.topicGaming,
+    '音乐' => l10n.topicMusic,
+    '城市' => l10n.topicCity,
+    '纪录' => l10n.topicDocumentary,
+    _ => topic,
+  };
+}
